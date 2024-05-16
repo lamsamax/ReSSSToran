213dfc11c9d01c2916b40eadeb34f72cb3856cda
Lamija <imamoviceva@gmail.com>
2024-05-16 18:42:43 +0200

    adding db

--- contact.php
+++ contact.php
@@ -1,10 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "imamoviceva@gmail.com";
+    $subject = "Message from $name";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email\r\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        $msg = "Message sent successfully!";
+    } else {
+        $msg = "Failed to send message.";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap" rel="stylesheet">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Web Page Title</title>
+    <title>ReSSSToran</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -29,16 +47,20 @@
         <button onclick="window.location.href='profile.html'">Profile</button>
     </div>
     <div class="wrapper">
-        <form class="form">
-            <div class="pageTitle title">Contact us </div>
-            <div class="secondaryTitle title">Please fill in all data in order to get response.</div>
-            <input type="text" class="name formEntry" placeholder="Name" />
-            <input type="text" class="email formEntry" placeholder="Email"/>
-            <input class="message formEntry" placeholder="Message"/>
-            <button type="button" class="submit formEntry">Submit</button>
+        <form class="form" action="contact.php" method="post">
+            <div class="pageTitle title">Contact us</div>
+            <div class="secondaryTitle title">Please fill in all data in order to get a response.</div>
+            <input type="text" name="name" class="name formEntry" placeholder="Name" required />
+            <input type="email" name="email" class="email formEntry" placeholder="Email" required />
+            <textarea name="message" class="message formEntry" placeholder="Message" required></textarea>
+            <button type="submit" class="submit formEntry">Submit</button>
         </form>
+        <?php
+        if (isset($msg)) {
+            echo "<p>$msg</p>";
+        }
+        ?>
     </div>
-
-</div>
 </div>
 </body>
+</html>
